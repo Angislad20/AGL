168d4c9d9bc0bd2e40b956bc724e5423e846b790
Angislad <koffiangisladhdv7@gmail.com>
2023-11-20 13:37:55 +0100

    premier push

---
+++ Backend/controllers/auth.js
@@ -0,0 +1,120 @@
+"use strict";
+const bcrypt = require('bcryptjs');
+
+const nodemailer = require("nodemailer");
+
+const User = require('../models/user');
+
+const transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "57fdf3a81d8bdf",
+    pass: "d874a0abd1ac3c"
+  }
+});
+
+
+
+
+
+exports.getLogin = (req, res, next) => {
+  let message = req.flash('erreur');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null
+  }
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    errorMessage: message
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash('erreur');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null
+  }
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    errorMessage: message
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        req.flash('erreur', 'Email ou mot de passe invalide')
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        req.flash('erreur', "L'email existe déja, veullez entrez une nouvelle adresse mail !")
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+          transporter.sendMail({
+            to: email,
+            from: 'lebook__shop.com',
+            subject: 'Notification',
+            html: '<h1>Votre inscrition est un succès total</h1>'
+          });
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
